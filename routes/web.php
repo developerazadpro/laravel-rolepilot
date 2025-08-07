6f5fe69875eb9284a29cb6094facc0b3cd123ada
developerazadpro <azharul.ece13.hstu@gmail.com>
2025-08-08 00:07:44 +0600

    Assign roles to user

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,6 +17,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/{id}/edit-role', [UserController::class, 'editRole'])->name('users.editRole');
+    Route::put('/users/{id}/update-role', [UserController::class, 'updateRole'])->name('users.updateRole');
 });
 
 Route::middleware(['auth', 'role:Admin'])->group(function () {
