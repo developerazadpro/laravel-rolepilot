9549f31cb3c722ea8340c8120c807c004c3b68c2
Md. Azharul Islam <azi@fiftytwo.com>
2025-10-30 17:28:15 +0600

    Profile updated alert showing

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -9,7 +9,15 @@
             {{ __('Profile') }}
         </h2>
     </x-slot>
-
+    @if (session('status'))
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-4">
+        <div class="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+            <span class="block sm:inline">
+                {{ __(session('status')) }}
+            </span>
+        </div>
+    </div>
+    @endif
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
